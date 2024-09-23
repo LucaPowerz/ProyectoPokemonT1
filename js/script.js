da265f6d8b5e6f7103eb82fba5112ba88f2a17c0
Lucapowerz <lucas.hdez2000@gmail.com>
2024-09-23 16:14:40 +0100

    primeros pasos

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+const button = document.querySelector("button")
 
+button.addEventListener("click", () => {
+    document.querySelectorAll("#filtro").forEach((e) => {
+        e.style.visibility = "visible"
+    });
+    document.querySelectorAll("#btn_lista_deseo").forEach( (e) => {
+        e.style.visibility = "visible"
+    });
+    document.querySelector(".listaPokemon").style.visibility = "visible";
+
+    mostrarPokemon();
+});
+
+function mostrarPokemon() {
+    document.querySelector(".cargandoDatos").style.visibility = "visible";
+}
